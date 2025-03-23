643c0fa53626c3353fffcb8e99de16c4e27b0e3b
tjyahl <thomasyahl@gmail.com>
2025-03-23 11:02:33 -0500

    created functioning vertical nav bar

--- index.js
+++ index.js
@@ -4,4 +4,18 @@
   Initiated 3/22/25
 */
 
-
+function handleClick(n) {
+    if (n==1) {
+	$("#research-dropdown").slideToggle("slow")
+	$("#teaching-dropdown").slideUp("slow")
+	$("#projects-dropdown").slideUp("slow")
+    } else if (n==2) {
+	$("#research-dropdown").slideUp("slow")
+	$("#teaching-dropdown").slideToggle("slow")
+	$("#projects-dropdown").slideUp("slow")
+    } else if (n==3) {
+	$("#research-dropdown").slideUp("slow")
+	$("#teaching-dropdown").slideUp("slow")
+	$("#projects-dropdown").slideToggle("slow")
+    }
+}
